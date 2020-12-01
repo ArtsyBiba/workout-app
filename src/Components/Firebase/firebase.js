6b1c42d9f19e30801cd95e5af60027a0ff9a0163
Artsiom Biba <biba.artsiom@gmail.com>
2020-12-01 10:27:55 -0500

    adding firebase

---
+++ src/Components/Firebase/firebase.js
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+var config = {
+    apiKey: "AIzaSyCPKXQn661Yu6_Ac0B0qjJJQ0HNdtBd5Js",
+    authDomain: "workout-app-a8d55.firebaseapp.com",
+    databaseURL: "https://workout-app-a8d55.firebaseio.com",
+    projectId: "workout-app-a8d55",
+    storageBucket: "workout-app-a8d55.appspot.com",
+    messagingSenderId: "958461883363",
+    appId: "1:958461883363:web:70a401cdcdfdad542066d6",
+    measurementId: "G-ZKGXFJJ3P9"
+  };
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+        this.auth = app.auth();
+        // this.db = app.database(); Later add database
+    }
+
+    /*** Authentication  ***/
+    doCreateUserWithEmailAndPassword = (email, password) => 
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) => 
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => 
+        this.auth.signOut();
+
+    doPasswordReset = email => 
+        this.auth.sendPasswordResetEmail(email);
+}
+
+export default Firebase;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { StylesProvider } from '@material-ui/core/styles';
 
 import './index.css';
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StylesProvider injectFirst>
-      <App />
-    </StylesProvider>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <StylesProvider injectFirst>
+        <App />
+      </StylesProvider>
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
