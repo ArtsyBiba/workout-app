9999c41694f25ace1fef14c16b34609c32806c74
Artsiom Biba <biba.artsiom@gmail.com>
2020-12-01 08:43:51 -0500

    adding signin page skeleton

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
-import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn'; 
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
-          Signin
+          <SignIn />
         </Route>
         <Route exact path='/signup'>
           Signup
