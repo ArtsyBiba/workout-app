c0eb2cb2ab7dbab85384df6ce1195797a564f2ef
Artsiom Biba <biba.artsiom@gmail.com>
2020-12-05 12:43:38 -0500

    rendering months

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -1,12 +1,32 @@
 import React, { useState } from 'react';
 import moment from 'moment';
 
+import './styles.css';
+
 export default function Calendar() {
     const value = moment();
     const startDay = value.clone().startOf('month').startOf('week');
     const endDay = value.clone().endOf('month').endOf('week');
     const day = startDay.clone().subtract(1, 'day');
-    const calendar = [];
+    const calendarArray = [];
+
+    while(day.isBefore(endDay, 'day')) {
+        calendarArray.push(
+            Array(7)
+                .fill(0)
+                .map(() => day.add(1, 'day').clone())
+        );
+    };
     
-    return <div>{day.format('MM/DD')}</div>
+    return (
+        <div className='calendar'>
+            {calendarArray.map((week) => (
+                <div>
+                    {week.map((day) => (
+                        <div className='day'>{day.format('D').toString()}</div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
 };
