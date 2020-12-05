f9c1633beb34732656b3db3dd68bae911a514807
Artsiom Biba <biba.artsiom@gmail.com>
2020-12-05 11:09:24 -0500

    setting up moment

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -3,7 +3,10 @@ import moment from 'moment';
 
 export default function Calendar() {
     const value = moment();
+    const startDay = value.clone().startOf('month').startOf('week');
+    const endDay = value.clone().endOf('month').endOf('week');
+    const day = startDay.clone().subtract(1, 'day');
+    const calendar = [];
     
-    
-    return <div>Calendar</div>
+    return <div>{day.format('MM/DD')}</div>
 };
