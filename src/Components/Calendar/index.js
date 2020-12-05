145042bcc928661f4082926276ee5cb55eee55b1
Artsiom Biba <biba.artsiom@gmail.com>
2020-12-05 13:04:48 -0500

    adding day selection

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -1,29 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
 import './styles.css';
 
 export default function Calendar() {
-    const value = moment();
+    const [calendar, setCalendar] = useState([]);
+    const [value, setValue] = useState(moment());
+
     const startDay = value.clone().startOf('month').startOf('week');
     const endDay = value.clone().endOf('month').endOf('week');
-    const day = startDay.clone().subtract(1, 'day');
-    const calendarArray = [];
 
-    while(day.isBefore(endDay, 'day')) {
-        calendarArray.push(
-            Array(7)
-                .fill(0)
-                .map(() => day.add(1, 'day').clone())
-        );
-    };
-    
+    useEffect(() => {
+        const day = startDay.clone().subtract(1, 'day');
+        const temp = [];
+
+        while(day.isBefore(endDay, 'day')) {
+            temp.push(
+                Array(7)
+                    .fill(0)
+                    .map(() => day.add(1, 'day').clone())
+            );
+        };
+
+        setCalendar(temp);
+    }, [value]);
+
+    // function dayStyles(day) {
+    //     if (beforeToday(day)) return 'before';
+    //     if (isSelected(day)) return 'selected';
+    //     if (isToday(day)) return 'today';
+    //     return '';
+    // }
+
     return (
         <div className='calendar'>
-            {calendarArray.map((week) => (
+            {calendar.map((week) => (
                 <div>
                     {week.map((day) => (
-                        <div className='day'>{day.format('D').toString()}</div>
+                        <div 
+                            className='day'
+                            onClick={() => setValue(day)}
+                        >
+                            <div className={value.isSame(day, 'day') ? 'selected' : ''}>
+                                {day.format('D').toString()}
+                            </div>
+                        </div>
                     ))}
                 </div>
             ))}
