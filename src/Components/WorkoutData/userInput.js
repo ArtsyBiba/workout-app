d3496b529ec70f710fef18b417273871fbcd8866
Artsy Biba <biba.artsiom@gmail.com>
2020-12-07 09:49:28 -0500

    updating userInput internal state

--- src/Components/WorkoutData/userInput.js
+++ src/Components/WorkoutData/userInput.js
@@ -3,7 +3,11 @@ import React, { useState } from 'react';
 import './styles.css';
 
 export default function UserInput(props) {
-    const [input, setInput] = useState('');
+    const [workout, setWorkout] = useState({
+        activity: '',
+        duration: '',
+        intensity: '',
+    });
 
     const handleClick = () => {
         
@@ -13,15 +17,21 @@ export default function UserInput(props) {
         <div className='user-input-wrapper'>
             <div className='user-input'>
                 <div className='input-type'>Activity Type</div>
-                <input onChange={(e) => setInput(e.target.value)} />
+                <input 
+                    onChange={(e) => setWorkout({...workout, activity: e.target.value})} 
+                />
             </div>
             <div className='user-input'>
                 <div className='input-type'>Duration</div>
-                <input onChange={(e) => setInput(e.target.value)} />
+                <input 
+                    onChange={(e) => setWorkout({...workout, duration: e.target.value})} 
+                />
             </div>
             <div className='user-input'>
                 <div className='input-type'>Intensity</div>
-                <input onChange={(e) => setInput(e.target.value)} />
+                <input 
+                    onChange={(e) => setWorkout({...workout, intensity: e.target.value})} 
+                />
             </div>
             <button className='submit-button' onClick={handleClick}>Add</button>
         </div>
