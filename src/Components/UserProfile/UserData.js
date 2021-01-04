b70cba9034ef222d0056b94042e11f55fc9b6af7
Artsy Biba <biba.artsiom@gmail.com>
2021-01-04 10:07:07 -0500

    including additional inputs

--- src/Components/UserProfile/UserData.js
+++ src/Components/UserProfile/UserData.js
@@ -5,38 +5,58 @@ import TextField from '@material-ui/core/TextField';
 
 export default function UserInput() {
     const defaultProfile = {
-        activity: '',
-        duration: '',
-        intensity: 5,
+        name: '',
+        age: '',
+        height: 0,
+        currentWeight: 150,
+        targetWeight: 150,
     };
 
     const [userProfile, setUserProfile] = useState(defaultProfile);
+
+    console.log(userProfile)
 
     return (
         <UserInputWrapper>
             <UserInputLine>
                 <StyledTextField 
-                    label='Activity Type'
-                    value={userProfile.activity}
-                    onChange={(e) => setUserProfile({...userProfile, activity: e.target.value})} 
+                    label='Full Name'
+                    value={userProfile.name}
+                    onChange={(e) => setUserProfile({...userProfile, name: e.target.value})} 
                 />
-                {userProfile && <SavedData>{userProfile.activity}</SavedData>}
+                {userProfile && <SavedData>{userProfile.name}</SavedData>}
             </UserInputLine>
             <UserInputLine>
                 <StyledTextField 
-                    label='Duration (min)'
-                    value={userProfile.duration}
-                    onChange={(e) => setUserProfile({...userProfile, duration: e.target.value})} 
+                    label='Age'
+                    value={userProfile.age}
+                    onChange={(e) => setUserProfile({...userProfile, age: e.target.value})} 
                 />
-                {userProfile && <SavedData>{userProfile.duration}</SavedData>}
+                {userProfile && <SavedData>{userProfile.age}</SavedData>}
             </UserInputLine>
             <UserInputLine>
                 <StyledTextField 
-                    label='Workout Goals'
-                    value={userProfile.duration}
-                    onChange={(e) => setUserProfile({...userProfile, duration: e.target.value})} 
+                    label='Height'
+                    value={userProfile.height}
+                    onChange={(e) => setUserProfile({...userProfile, height: e.target.value})} 
                 />
-                {userProfile && <SavedData>{userProfile.intensity}</SavedData>}
+                {userProfile && <SavedData>{userProfile.height}</SavedData>}
+            </UserInputLine>
+            <UserInputLine>
+                <StyledTextField 
+                    label='Current Weight'
+                    value={userProfile.currentWeight}
+                    onChange={(e) => setUserProfile({...userProfile, currentWeight: e.target.value})} 
+                />
+                {userProfile && <SavedData>{userProfile.currentWeight} lbs</SavedData>}
+            </UserInputLine>
+            <UserInputLine>
+                <StyledTextField 
+                    label='Target Weight'
+                    value={userProfile.targetWeight}
+                    onChange={(e) => setUserProfile({...userProfile, targetWeight: e.target.value})} 
+                />
+                {userProfile && <SavedData>{userProfile.targetWeight} lbs</SavedData>}
             </UserInputLine>
         </UserInputWrapper>
     )
