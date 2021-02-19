a2f437be02ad7793e8b3ecbd14db3d651d9ded1e
Artsy Biba <biba.artsiom@gmail.com>
2021-02-18 20:23:08 -0500

    rendering basic data with d3

--- src/Components/Data/Table.js
+++ src/Components/Data/Table.js
@@ -1,5 +1,21 @@
+import * as d3 from "d3";
+
 export default function Table () {
+    const DUMMY_DATA = [
+        { id: 'd1', value: 10, region: 'USA' },
+        { id: 'd2', value: 11, region: 'Canada' },
+        { id: 'd3', value: 13, region: 'Germany' },
+        { id: 'd4', value: 6, region: 'India' },
+    ];
+    
+    d3.select('#data')
+        .selectAll('p')
+        .data(DUMMY_DATA)
+        .enter()
+        .append('p')
+        .text(dta => dta.region);
+
     return (
-        <div>Data</div>
+        <div id='data'></div>
     )
 }
